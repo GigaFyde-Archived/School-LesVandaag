5787c6bb60a3f9799e7939a0b790c1ca2f45c626
Laurant Marijnissen <laurant@gigafyde.dev>
2021-03-21 21:42:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LesVandaag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime vandaag = DateTime.Now;
+            int day = (int)DateTime.Now.DayOfWeek;
+            string uitvoer = "";
+            switch (day.ToString()) {
+                case "0":
+                    {
+                        uitvoer = "Geen les"; break;
+                    }
+                case "1": 
+                    { 
+                        uitvoer = "C# lessen"; break; 
+                    } 
+                case "2": 
+                    { 
+                        uitvoer = "Project"; break; 
+                    } 
+                case "3": 
+                    { 
+                        uitvoer = "Keuzedeel"; break; 
+                    } 
+                case "4": 
+                    { 
+                        uitvoer = "HTLM-CSS"; break; 
+                    } 
+                case "5": 
+                    { 
+                        uitvoer = "Python"; break; 
+                    }
+                case "6":
+                    {
+                        uitvoer = "Geen les"; break;
+                    }
+            }
+            Console.WriteLine("Op " + vandaag.DayOfWeek + " heb je " + uitvoer + "."); Console.ReadKey();
+
+        }
+    }
+}
